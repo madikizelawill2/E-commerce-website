6b41f8891aeecd0043cf0a47d95fcd2e1a66729f
Will Madikizela <63541852+madikizelawill2@users.noreply.github.com>
2022-03-12 14:53:24 +0200

    Add files via upload

---
+++ public/js/data.js
@@ -0,0 +1,311 @@
+//#region PRODUCT DATA
+const BIG_SALE_PRODUCTS = [
+  {
+    id: 0,
+    name: 'Brand',
+    imageUrl: 'img/card1.jpg',
+    descriptipon: 'description',
+    price: 217,
+    sale_price: 100,
+  },
+  {
+    id: 1,
+    name: 'Brand',
+    imageUrl: 'img/card2.jpg',
+    descriptipon: 'description',
+    price: 300,
+    sale_price: 265,
+  },
+  {
+    id: 2,
+    name: 'Brand',
+    imageUrl: 'img/card3.jpg',
+    descriptipon: 'description',
+    price: 900,
+    sale_price: 768,
+  },
+  {
+    id: 3,
+    name: 'Brand',
+    imageUrl: 'img/card4.jpg',
+    descriptipon: 'description',
+    price: 400,
+    sale_price: 350,
+  },
+  {
+    id: 4,
+    name: 'Brand',
+    imageUrl: 'img/card5.jpg',
+    descriptipon: 'description',
+    price: 650,
+    sale_price: 300,
+  },
+];
+
+const NEW_ARRIVAL_PRODUCTS = [
+  {
+    id: 5,
+    name: 'Brand',
+    imageUrl: 'img/NA1.jpg',
+    descriptipon: 'description',
+    price: 217,
+  },
+  {
+    id: 6,
+    name: 'Brand',
+    imageUrl: 'img/NA2.jpg',
+    descriptipon: 'description',
+    price: 300,
+  },
+  {
+    id: 7,
+    name: 'Brand',
+    imageUrl: 'img/NA3.jpg',
+    descriptipon: 'description',
+    price: 900,
+  },
+  {
+    id: 8,
+    name: 'Brand',
+    imageUrl: 'img/NA4.jpg',
+    descriptipon: 'description',
+    price: 400,
+  },
+  {
+    id: 9,
+    name: 'Brand',
+    imageUrl: 'img/NA5.jpg',
+    descriptipon: 'description',
+    price: 650,
+  },
+];
+
+const TRENDING_PRODUCTS = [
+  {
+    id: 10,
+    name: 'Tredding',
+    imageUrl: 'img/T1.jpg',
+    descriptipon: 'description',
+    price: 217,
+  },
+  {
+    id: 11,
+    name: 'Tredding',
+    imageUrl: 'img/T2.jpg',
+    descriptipon: 'description',
+    price: 300,
+  },
+  {
+    id: 12,
+    name: 'Tredding',
+    imageUrl: 'img/T3.jpg',
+    descriptipon: 'description',
+    price: 900,
+  },
+  {
+    id: 13,
+    name: 'Tredding',
+    imageUrl: 'img/T4.jpg',
+    descriptipon: 'description',
+    price: 400,
+  },
+  {
+    id: 14,
+    name: 'Tredding',
+    imageUrl: 'img/T5.jpg',
+    descriptipon: 'description',
+    price: 650,
+  },
+];
+
+const WOMEN_PRODUCTS = [
+  {
+    id: 15,
+    name: 'Women Brand',
+    imageUrl: 'img/W1.jpg',
+    descriptipon: 'description',
+    price: 217,
+  },
+  {
+    id: 16,
+    name: 'Women Brand',
+    imageUrl: 'img/W2.jpg',
+    descriptipon: 'description',
+    price: 300,
+  },
+  {
+    id: 17,
+    name: 'Women Brand',
+    imageUrl: 'img/W3.jpg',
+    descriptipon: 'description',
+    price: 900,
+  },
+  {
+    id: 18,
+    name: 'Women Brand',
+    imageUrl: 'img/W4.jpg',
+    descriptipon: 'description',
+    price: 400,
+  },
+  {
+    id: 19,
+    name: 'Women Brand',
+    imageUrl: 'img/W5.jpg',
+    descriptipon: 'description',
+    price: 650,
+  },
+];
+
+const MEN_PRODUCTS = [
+  {
+    id: 20,
+    name: 'Men Brand',
+    imageUrl: 'img/M1.jpg',
+    descriptipon: 'description',
+    price: 217,
+  },
+  {
+    id: 21,
+    name: 'Men Brand',
+    imageUrl: 'img/M2.jpg',
+    descriptipon: 'description',
+    price: 300,
+  },
+  {
+    id: 22,
+    name: 'Men Brand',
+    imageUrl: 'img/M3.jpg',
+    descriptipon: 'description',
+    price: 900,
+  },
+  {
+    id: 23,
+    name: 'Men Brand',
+    imageUrl: 'img/M4.jpg',
+    descriptipon: 'description',
+    price: 400,
+  },
+  {
+    id: 24,
+    name: 'Men Brand',
+    imageUrl: 'img/M5.jpg',
+    descriptipon: 'description',
+    price: 650,
+  },
+];
+
+const KIDS_PRODUCTS = [
+  {
+    id: 25,
+    name: 'Kids Brand',
+    imageUrl: 'img/K1.jpg',
+    descriptipon: 'description',
+    price: 217,
+  },
+  {
+    id: 26,
+    name: 'Kids Brand',
+    imageUrl: 'img/K2.jpg',
+    descriptipon: 'description',
+    price: 300,
+  },
+  {
+    id: 27,
+    name: 'Kids Brand',
+    imageUrl: 'img/K3.jpg',
+    descriptipon: 'description',
+    price: 900,
+  },
+  {
+    id: 28,
+    name: 'Kids Brand',
+    imageUrl: 'img/K4.jpg',
+    descriptipon: 'description',
+    price: 400,
+  },
+  {
+    id: 29,
+    name: 'Kids Brand',
+    imageUrl: 'img/K5.jpg',
+    descriptipon: 'description',
+    price: 650,
+  },
+];
+
+const ACCESSORIES_PRODUCTS = [
+  {
+    id: 30,
+    name: 'Accessories Brand',
+    imageUrl: 'img/ACC1.jpg',
+    descriptipon: 'description',
+    price: 217,
+  },
+  {
+    id: 31,
+    name: 'Accessories Brand',
+    imageUrl: 'img/ACC2.jpg',
+    descriptipon: 'description',
+    price: 300,
+  },
+  {
+    id: 32,
+    name: 'Accessories Brand',
+    imageUrl: 'img/ACC3.jpg',
+    descriptipon: 'description',
+    price: 900,
+  },
+  {
+    id: 33,
+    name: 'Accessories Brand',
+    imageUrl: 'img/ACC4.jpg',
+    descriptipon: 'description',
+    price: 400,
+  },
+  {
+    id: 34,
+    name: 'Accessories Brand',
+    imageUrl: 'img/ACC5.jpg',
+    descriptipon: 'description',
+    price: 650,
+  },
+];
+
+const SHOES_PRODUCTS = [
+  {
+    id: 35,
+    name: 'Shoe Brand',
+    imageUrl: 'img/S1.jpg',
+    descriptipon: 'description',
+    price: 217,
+  },
+  {
+    id: 36,
+    name: 'Shoe Brand',
+    imageUrl: 'img/S2.jpg',
+    descriptipon: 'description',
+    price: 300,
+  },
+  {
+    id: 37,
+    name: 'Shoe Brand',
+    imageUrl: 'img/S3.jpg',
+    descriptipon: 'description',
+    price: 900,
+  },
+  {
+    id: 38,
+    name: 'Shoe Brand',
+    imageUrl: 'img/S4.jpg',
+    descriptipon: 'description',
+    price: 400,
+  },
+  {
+    id: 39,
+    name: 'Shoe Brand',
+    imageUrl: 'img/S5.jpg',
+    descriptipon: 'description',
+    price: 650,
+  },
+];
+
+//#endregion PRODUCT DATA
